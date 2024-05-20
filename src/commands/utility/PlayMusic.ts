840808fa8aa94faf37e53d0eb3ab89a796ada599
Awake <piraveen05@gmail.com>
2024-05-20 13:40:41 +0100

    General progress

--- src/commands/utility/PlayMusic.ts
+++ src/commands/utility/PlayMusic.ts
@@ -1,21 +1,92 @@
-import { SlashCommandBuilder, CommandInteraction, GuildMember } from "discord.js";
-import { playTrack } from "../../utils/MusicPlayer";
+/***********************************************/
+/*                                             */
+/*     This file is merely a state manager     */
+/*                                             */
+/*     Audio data from here will be played     */
+/*         from "./utils/VcManager.ts"         */
+/*                                             */
+/***********************************************/
 
+
+import { SlashCommandBuilder, CommandInteraction, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, TextBasedChannel, InteractionReplyOptions } from "discord.js";
+import { joinVc } from "../../utils/VcManager";
+
+/********** UI Elements **********/
+const playButton = new ButtonBuilder()
+    .setCustomId("play")
+    .setLabel("Play")
+    .setStyle(ButtonStyle.Success);
+
+const pauseButton = new ButtonBuilder()
+    .setCustomId("pause")
+    .setLabel("Pause")
+    .setStyle(ButtonStyle.Secondary);
+
+const nextButton = new ButtonBuilder()
+    .setCustomId("nextButton")
+    .setLabel("Next")
+    .setStyle(ButtonStyle.Secondary);
+
+const controlPanelEmbed = new EmbedBuilder()
+    .setTitle("Music Control Panel")
+    .setDescription("Now Playing: ");
+
+const buttonRow = new ActionRowBuilder()
+    .setComponents(playButton, nextButton);
+
+/**********    Logic    **********/
+class Instance {
+    private tracks: string[] = [];
+    private interaction: CommandInteraction | undefined;
+    private controlPanel: InteractionReplyOptions = { embeds: [controlPanelEmbed], components: [buttonRow as any] };
+
+    constructor(interaction: CommandInteraction) {
+        this.interaction = interaction;
+    }
+
+    public addTrack(url: string): void {
+        this.tracks.push(url);
+    }
+
+    public sendControlPanel(): void {
+        this.interaction?.reply(this.controlPanel);
+    }
+}
+
+const instances: Map<string, Instance> = new Map();
 const data = new SlashCommandBuilder()
     .setName("play")
-    .setDescription("Play music in a vc from YouTube.");
+    .setDescription("Play music in a vc from YouTube.")
+    .addStringOption(option => option
+        .setName("url")
+        .setDescription("Youtube URL")
+        .setRequired(true)
+    );
+
+function play() {
+
+}
 
 async function execute(interaction: CommandInteraction) {
-    console.log(await interaction.guild?.members.fetch(interaction.user));
-    const voiceChannel = null; // interaction.member.voice.channel;
-    if (voiceChannel) {
-        playTrack(voiceChannel);
-    } else {
+    const voiceChannel = (await interaction.guild?.members.fetch(interaction.user))?.voice.channel;
+
+    if (!voiceChannel) {
         await interaction.reply({
             ephemeral: true,
             content: "You must be in a VC to use this command."
         });
+
+        return;
     }
+
+    joinVc(voiceChannel);
+    let instance: Instance | undefined = instances.get(interaction.guildId || "");
+    if (!instance) {
+        instance = new Instance(interaction);
+        instances.set(interaction.guildId as string, instance);
+    }
+    const url = interaction.options.get("url")?.value as string;
+    instance.addTrack(url);
 }
 
 export { data, execute };
--- src/utils/VcManager.ts
+++ src/utils/VcManager.ts
@@ -1,18 +1,48 @@
-import { VoiceConnection, joinVoiceChannel } from "@discordjs/voice";
+import { AudioPlayer, AudioResource, PlayerSubscription, VoiceConnection, createAudioPlayer, createAudioResource, joinVoiceChannel } from "@discordjs/voice";
 import { GuildMember, VoiceBasedChannel } from "discord.js";
+import { log } from "../services/Api";
 
-const voiceConnections: Map<string, VoiceConnection> = new Map();
+class Stream {
+    private connection: VoiceConnection | undefined;
+    private subscription: PlayerSubscription | undefined;
+    private player: AudioPlayer = createAudioPlayer();
+    private resource: AudioResource | undefined;
+    private guildName: string = "";
+    private vcName: string = "";
 
-export function joinVc(voiceChannel: VoiceBasedChannel) {
-    voiceConnections.set(voiceChannel.guildId, joinVoiceChannel({
-        channelId: voiceChannel.id,
-        guildId: voiceChannel.guildId,
-        adapterCreator: voiceChannel.guild.voiceAdapterCreator
-    }));
+    constructor(vc: VoiceBasedChannel) {
+        this.connection = joinVoiceChannel({
+            channelId: vc.id,
+            guildId: vc.guildId,
+            adapterCreator: vc.guild.voiceAdapterCreator
+        });
+
+        this.connection.subscribe(this.player);
+        this.guildName = vc.guild.name;
+        this.vcName = vc.name;
+    }
+
+    private onReady(): void {
+        log(`Connect and ready to stream audio to [${this.guildName} -> ${this.vcName}]`);
+    }
+
+    private onDisconnected(): void {
+        log(`Disconnected from [${this.guildName} -> ${this.vcName}]`);
+    }
+
+    public diconnect(): void {
+        this.connection?.destroy();
+    }
+}
+
+const voiceConnections: Map<string, Stream> = new Map();
+
+export function joinVc(vc: VoiceBasedChannel) {
+    voiceConnections.set(vc.guildId, new Stream(vc));
 }
 
 export function leaveVc(guildId: string) {
     const connection = voiceConnections.get(guildId);
-    if (connection) connection.destroy();
+    if (connection) connection.diconnect();
     voiceConnections.delete(guildId);
 }
